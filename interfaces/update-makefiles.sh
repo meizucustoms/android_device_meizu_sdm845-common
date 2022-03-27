8660c1db93d3cf678b4c477bbdf95ea9df790158
TRANSAERO21 <martynenkodima21@gmail.com>
2022-03-27 14:43:46 +0300

    sdm845-common: interfaces: update regen script and run it

--- interfaces/update-makefiles.sh
+++ interfaces/update-makefiles.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
+set -e
 
-do_makefiles_update \
-  "vendor.synaptics:device/meizu/sdm845-common/interfaces/synaptics"
-  "vendor.goodix:device/meizu/sdm845-common/interfaces/goodix"
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../../.."
+HELPER="${ANDROID_ROOT}/system/tools/hidl/update-makefiles-helper.sh"
+
+MAKEFILES_LIST="\
+  vendor.synaptics:device/meizu/sdm845-common/interfaces/synaptics\
+  vendor.goodix:device/meizu/sdm845-common/interfaces/goodix"
+
+source ${HELPER}
+
+for makefile in ${MAKEFILES_LIST}
+do
+  do_makefiles_update ${makefile}
+done
+
+for file in $(find ${MY_DIR} -name "*.bp")
+do
+  sed -i '/system_ext_specific: true/d' ${file}
+  sed -i '/gen_java: true/d' ${file}
+done
